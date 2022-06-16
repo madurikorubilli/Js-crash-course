3ee8a3b4bdbf71357f1d3da2f6b2fd58e517d90b
Maduri <madurikorubilli@gmail.com>
2022-06-16 18:46:32 +0530

    first commit

---
+++ main.js
@@ -0,0 +1,7 @@
+const ul=document.querySelector('.Items');
+
+//ul.remove();
+//ul.lastElementChild.remove();
+ul.lastElementChild.textcontent='Hello';
+ul.children[1].innertext ='Brad';
+ul.lastElementChild.innerHTML ='<h1>Hello</h1>';
